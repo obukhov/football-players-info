2ec35629461aba5fc961b97db703549e47401267
Alex Obukhov <a.a.obukhov@gmail.com>
2016-11-09 01:33:20 +0100

    Get parameters from env and args or stdin

--- main.go
+++ main.go
@@ -7,30 +7,15 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	// init and run api getter
-	// init and run data processor
-	// format data
-	teamNames := []string{
-		//"ololo",
-		"England",
-		"Germany",
-		"England",
-		"France",
-		"Spain",
-		"Manchester Utd",
-		"Arsenal",
-		"Chelsea",
-		"Barcelona",
-		"Real Madrid",
-		"FC Bayern Munich",
-	}
+	teamNames := getTeamNames()
+	threads, maxIdLimit := getEnvConfig()
 
-	threads := 10
-
-	reader := multithread.NewMultiThreadApiReader(api.NewApiClient(), threads, 500)
+	reader := multithread.NewMultiThreadApiReader(api.NewApiClient(), threads, maxIdLimit)
 	processor := aggregator.NewAggregator()
 	searcher := multithread.NewSearcher(teamNames, reader, processor)
 
@@ -50,4 +35,47 @@ func main() {
 
 	logger.Println("Done! Result: found")
 	processor.Collection().Output(os.Stdout)
+}
+func getEnvConfig() (int, int) {
+	threads, err := strconv.Atoi(os.Getenv("API_THREADS"))
+	if nil != err || threads < 1 {
+		threads = 10
+	}
+
+	maxIdLimit, err := strconv.Atoi(os.Getenv("MAX_ID_LIMIT"))
+	if nil != err || maxIdLimit < 1 {
+		maxIdLimit = 500
+	}
+
+	return threads, maxIdLimit
+}
+
+func getTeamNames() []string {
+
+	if len(os.Args) > 1 {
+		return os.Args[1:]
+	}
+
+	stat, _ := os.Stdin.Stat()
+	if (stat.Mode() & os.ModeCharDevice) == 0 {
+		byteIn, err := ioutil.ReadAll(os.Stdin)
+		teamNames := strings.Split(string(byteIn), "\n")
+		if nil == err || len(teamNames) > 0 {
+			return teamNames
+		}
+	}
+
+	return []string{
+		"England",
+		"Germany",
+		"England",
+		"France",
+		"Spain",
+		"Manchester Utd",
+		"Arsenal",
+		"Chelsea",
+		"Barcelona",
+		"Real Madrid",
+		"FC Bayern Munich",
+	}
 }
